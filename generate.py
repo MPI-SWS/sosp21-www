37a7d11a0cebbb34c30c40c24fb7145c1dff644b
Ali Mashtizadeh <ali@mashtizadeh.com>
2018-06-07 12:36:46 -0400

    Initial commit of SOSP 19

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 #
 
 import csv
-import markdown
+#import markdown
 from jinja2 import Environment, FileSystemLoader, Markup, select_autoescape
 
 env = Environment(
@@ -11,83 +11,7 @@ env = Environment(
         autoescape=select_autoescape(['html', 'xml'])
         )
 
-#
-# Home Page
-#
-
-# Parse Markdown
-index_md = open("index.md", "r")
-indexcontent = markdown.markdown(index_md.read(),
-                                 extensions = ['extra', 'smarty'], 
-                                 output_format='html5')
-
 # Parse Template
 template = env.get_template('index.html')
 index_file = open("index.html", "w")
-index_file.write(template.render(page = "home",
-                                 title = "",
-                                 indextext = Markup(indexcontent)))
-
-#
-# People Page
-#
-
-# Parse CSV
-people_csv = open("people.csv", "r")
-people = csv.reader(people_csv,
-                    delimiter = ",",
-                    quotechar = '"',
-                    skipinitialspace = True)
-grouping = env.get_template('grouping.html')
-person = env.get_template('person.html')
-pc = ""
-curgroup = ""
-for r in people:
-    if (len(r) == 0) or (r[0].startswith("#")):
-        # Ignore empty lines and comments
-        pass
-    elif (r[0].startswith("@")):
-        pc += "<div class=\"row\">"
-        pc += curgroup
-        pc += "</div>"
-        pc += "<div class=\"row\"><h2>&nbsp;</h2></div>"
-        pc += grouping.render(group = r[0][1:])
-        curgroup = ""
-    else:
-        u = ""
-        if (r[1].startswith("~")):
-            u = "https://cs.uwaterloo.ca/" + r[1]
-        else:
-            u = r[1]
-        curgroup += person.render(name = r[0], url = u, photo = r[2])
-pc += "<div class=\"row\">"
-pc += curgroup
-pc += "</div>"
-
-# Parse Template
-template = env.get_template('people.html')
-people_file = open("people.html", "w")
-people_file.write(template.render(page = "people",
-                                  title = ": People",
-                                  peopletext = Markup(pc)))
-
-#
-# Seminar Page
-#
-
-#
-# Publications Page
-#
-
-# Parse Input
-pubs = open("bibtex/all.html").read()
-
-# Parse Template
-template = env.get_template('publications.html')
-pub_file = open("publications.html", "w")
-pub_file.write(template.render(page = "pubs",
-                               title = "",
-                               indextext = Markup(pubs)))
-
-#
-
+index_file.write(template.render(page = "home", title = ""))
